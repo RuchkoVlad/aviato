a7c2072b8c4f053bb7bcbaa2d120cc3833f848ff
Admin <admin@MacBook-Pro-Admin.local>
2019-04-19 18:59:36 +0300

    test

---
+++ README.md
@@ -0,0 +1,13 @@
+# Aviasales
+
+### [Demo](https://link-to-github-pages.com)
+
+### Main page of site Aviasales
+
+Training BEM
+
+### Technologies are used
+
+- HTML
+- CSS
+- BEM(without modification)
---
+++ src/js.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('.partners__carousel').slick({
+    slidesToShow: 4,
+    prevArrow: '<div class="button-left"></div>',
+    nextArrow: '<div class="button-right"></div>',
+  });
+});
